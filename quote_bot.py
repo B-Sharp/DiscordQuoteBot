c6123d67b27d0e991d1161709f237b71f2e5c3d4
Brian Sharp <briansharp628@gmail.com>
2019-12-18 00:44:03 -0500

    Combined the sounds.json into quotes.json and updated the script accordingly

--- quote_bot.py
+++ quote_bot.py
@@ -2,41 +2,40 @@ import json
 import discord
 import random
 
-# loads a list of quotes from quotes.json
-with open("quotes.json", "r") as qf:
-    quotes_json = json.load(qf)
-    quotes = quotes_json["quotes"]
+def main():
+    # loads a list of quotes from quotes.json
+    with open("quotes.json", "r") as qf:
+        quotes_json = json.load(qf)
+        quotes = quotes_json["quotes"]
 
-# loads bot token from config.json
-with open("config.json", "r") as cf:
-    config_json = json.load(cf)
-    bot_token = config_json["token"]
+    # loads bot token from config.json
+    with open("config.json", "r") as cf:
+        config_json = json.load(cf)
+        bot_token = config_json["token"]
 
-# loads a list of sound filenames from sounds.json
-with open("sounds.json", "r") as sf:
-    sounds_json = json.load(sf)
-    sounds = sounds_json["sounds"]
+    # create a client
+    client = discord.Client()
 
-# pairs each quote with its sound filename
-mapped_pairs = list(zip(quotes, sounds))
+    # register a message event listener
+    @client.event
+    async def on_message(msg):
+        # ignore messages from itself and other bots
+        if msg.author == client.user or msg.author.bot:
+            return
+        
+        # if the bot is mentioned select a random quote and send it then play the audio for that quote
+        for mention in msg.mentions:
+            if mention == client.user:
+                random_quote = random.choice(list(quotes))
+                
+                await msg.channel.send(random_quote)
+                
+                if quotes[random_quote]:
+                    voice_client = await msg.author.voice.channel.connect();
+                    voice_client.play(discord.FFmpegPCMAudio("sounds\\" + quotes[random_quote]), after=lambda e: voice_client.loop.create_task(voice_client.disconnect()))
 
-# create a client
-client = discord.Client()
+    # run the bot
+    client.run(bot_token)
 
-# register a message event listener
-@client.event
-async def on_message(msg):
-    # ignore messages from itself and other bots
-    if msg.author == client.user or msg.author.bot:
-        return
-    
-    # if the bot is mentioned select a random quote and send it
-    for mention in msg.mentions:
-        if mention == client.user:
-            random_pair = random.choice(mapped_pairs)
-            await msg.channel.send(random_pair[0])
-            voice_client = await msg.author.voice.channel.connect();
-            voice_client.play(discord.FFmpegPCMAudio("sounds\\" + random_pair[1]), after=lambda e: voice_client.loop.create_task(voice_client.disconnect()))
-
-# run the bot
-client.run(bot_token)
+if __name__ == "__main__":
+    main()
